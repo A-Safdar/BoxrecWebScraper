e4bc0e306f57bee8c22956c783a37fbe2347b9e5
bongla316 <67811571+A-Safdar@users.noreply.github.com>
2022-02-21 17:40:24 +0000

    refactored scraper to use config file and support multiple pages

--- webscraper.py
+++ webscraper.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
+import json
 import pandas as pd
 
 
@@ -12,21 +13,23 @@ class WebDriver:
     Attributes
     ----------
 
-    address :   str
-                The url of the website to be scraped
+    configuration_path :    str
+                            The path for the json file which contains the url, username and password
     '''
     """
 
-    def __init__(self, address: str, username: str, password: str):
+    def __init__(self, configuration_path: str):
         # Initialise a webdriver when class is called
         # Delete all cookies
         self.driver = webdriver.Firefox()
         self.driver.delete_all_cookies()
+        self.configuration_path = configuration_path
 
         # Required attributes when initialising
-        self.address = address
-        self.username = username
-        self.password = password
+        scraper_config = self.__get_scraper_settings()
+        self.address = scraper_config["url"]
+        self.username = scraper_config["username"]
+        self.password = scraper_config["password"]
 
         # Load the url
         self.driver.get(self.address)
@@ -47,6 +50,37 @@ class WebDriver:
 
         # Utility attributes used throughout the class
         self.table_row_data = []
+
+    def __get_scraper_settings(self):
+        """
+        PRIVATE method used to extract relevant settings from the json config file
+        """
+        with open(self.configuration_path, "r") as f:
+            return json.load(f)
+
+    def __load_page(self, url: str):
+        """
+        PRIVATE method used to load a page with a specific url and wait 2 seconds once the request is made
+        '''
+        Attributes
+        ----------
+
+        url:    str
+                The url of the page to load
+        """
+        self.driver.get(url)
+        sleep(2)
+
+    def __extract_current_page_data(self):
+        """
+        PRIVATE method used to get all the relevant data within a specific page
+        """
+        # Clear the table row data array every time this method is called
+        self.table_row_data = []
+        ratings_table = self.driver.find_element(By.ID, "ratingsResults")
+        table_rows = ratings_table.find_elements(By.TAG_NAME, "tr")
+        for row in table_rows:
+            self.table_row_data.append(row.find_elements(By.TAG_NAME, "td"))
 
     def accept_cookies(self):
         """
@@ -71,29 +105,16 @@ class WebDriver:
 
         # Enter username and passwords into fields and click login
         # TODO: ADD SUPPORT FOR GETTING CREDENTIALS FROM JSON
-        form_username.send_keys("")
-        form_password.send_keys("")
+        form_username.send_keys(self.username)
+        form_password.send_keys(self.password)
         form_submit.click()
 
-    def load_current_rankings_page(self):
+    def load_rankings_page(self):
         """
         Method to navigate to the rankings page
         """
         ratings_link = self.driver.find_element(By.LINK_TEXT, "ratings")
         ratings_link.click()
-
-    def load_page(self, url: str):
-        """
-        Method used to load a page with a specific url and wait 2 seconds once the request is made
-        '''
-        Attributes
-        ----------
-
-        url:    str
-                The url of the page to load
-        """
-        self.driver.get(url)
-        sleep(2)
 
     def build_list_of_page_links(self, number_of_pages: int):
         """
@@ -114,53 +135,48 @@ class WebDriver:
         for page in lst_page_offset:
             self.basic_links_list.append(f"{template_url}{page}")
 
-    def extract_table_row_data(self):
-        """
-        Method used to get all the data within each row of the table for each page
-        """
-        for count, link in enumerate(self.basic_links_list):
-            # First page is already loaded so no need to reload
-            if count != 0:
-                self.load_page(link)
-
-            ratings_table = self.driver.find_element(By.ID, "ratingsResults")
-            table_rows = ratings_table.find_elements(By.TAG_NAME, "tr")
-            for row in table_rows:
-                self.table_row_data.append(row.find_elements(By.TAG_NAME, "td"))
-
     def build_basic_information_lists(self):
         """
-        Method to process each bit of data and add each bit to its relevant list
+        Method used to begin scraping through the website and through different pages
         """
-        for row in self.table_row_data:
-            for count, data in enumerate(row):
-                if count == 0:
-                    if bool(data.text):
-                        self.basic_ranking_list.append(data.text)
+        for page_number, link in enumerate(self.basic_links_list):
+            # First page at this point is already loaded so we can ignore
+            if page_number != 0:
+                self.__load_page(link)
 
-                elif count == 1:
-                    self.basic_name_list.append(data.text)
-                    more_details_link = data.find_element(By.TAG_NAME, "a")
-                    self.basic_more_details.append(more_details_link.get_attribute("href"))
+            # Process current pages data
+            self.__extract_current_page_data()
 
-                elif count == 2:
-                    self.basic_points_list.append(data.text)
+            for row in self.table_row_data:
+                for count, data in enumerate(row):
+                    if count == 0:
+                        if bool(data.text):
+                            self.basic_ranking_list.append(data.text)
 
-                elif count == 4:
-                    self.basic_division_list.append(data.text)
+                    elif count == 1:
+                        self.basic_name_list.append(data.text)
+                        more_details_link = data.find_element(By.TAG_NAME, "a")
+                        self.basic_more_details.append(more_details_link.get_attribute("href"))
 
-                elif count == 5:
-                    self.basic_age_list.append(data.text)
+                    elif count == 2:
+                        self.basic_points_list.append(data.text)
 
-                elif count == 6:
-                    self.basic_record_list.append(data.text)
+                    elif count == 4:
+                        self.basic_division_list.append(data.text)
 
-                elif count == 8:
-                    self.basic_stance_list.append(data.text)
+                    elif count == 5:
+                        self.basic_age_list.append(data.text)
 
-                elif count == 9:
-                    self.basic_residence_list.append(data.text)
+                    elif count == 6:
+                        self.basic_record_list.append(data.text)
 
+                    elif count == 8:
+                        self.basic_stance_list.append(data.text)
+
+                    elif count == 9:
+                        self.basic_residence_list.append(data.text)
+
+        # Once all data has been retrieved, generate a unique id for each entry
         self.create_id_from_link()
 
     def create_id_from_link(self):
@@ -187,17 +203,23 @@ class WebDriver:
 
         self.rankings_table = pd.DataFrame.from_dict(basic_rankings_info)
 
+    def write_dataframe_to_csv(self):
+        """
+        Method used to create a csv file of all the data that was scraped
+        """
+        self.rankings_table.to_csv("test.csv")
+
 
 if __name__ == "__main__":
-    scraper = WebDriver(address="https://boxrec.com", username="", password="")
+    scraper = WebDriver(configuration_path="credentials.json")
     sleep(2)
     scraper.login()
     sleep(2)
     scraper.accept_cookies()
     sleep(2)
-    scraper.load_current_rankings_page()
+    scraper.load_rankings_page()
     sleep(2)
-    scraper.build_list_of_page_links(50)
-    scraper.extract_table_row_data()
+    scraper.build_list_of_page_links(3)
     scraper.build_basic_information_lists()
     scraper.create_basic_info_dataframe()
+    scraper.write_dataframe_to_csv()
