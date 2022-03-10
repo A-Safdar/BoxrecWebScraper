ec86394e68db545eb179b47ffeb5bb636735f90a
bongla316 <67811571+A-Safdar@users.noreply.github.com>
2022-03-10 20:25:42 +0000

    added unit test for accept_cookies public method

--- scraper/tests/test_webscraper.py
+++ scraper/tests/test_webscraper.py
@@ -4,6 +4,7 @@ import json
 import sys
 import os
 from selenium.webdriver.common.by import By
+import selenium.common.exceptions as driver_exceptions
 
 # Get current and parent directories to allow for imports
 current = os.path.dirname(os.path.realpath(__file__))
@@ -36,6 +37,31 @@ class WebScraperTestCase(unittest.TestCase):
         expected_result = "https://boxrec.com/"
         actual_result = self.instance.driver.current_url
         self.assertEqual(expected_result, actual_result)
+
+    def test_accept_cookies(self):
+        sleep(2)
+        self.instance.load_login_page(By.LINK_TEXT, "login")
+        self.instance.submit_login_credentials(
+            username_field_by=By.ID,
+            username_field_locator="username",
+            password_field_by=By.ID,
+            password_field_locator="password",
+            submit_button_by=By.CLASS_NAME,
+            submit_button_locator="submitButton",
+        )
+        self.instance.accept_cookies(
+            container_by=By.XPATH,
+            container_locator_string='//div[@id="qc-cmp2-ui"]',
+            button_by=By.XPATH,
+            button_locator_string='//button[contains(@class, "css-1my9mvs")]',
+        )
+
+        self.assertRaises(
+            driver_exceptions.NoSuchElementException,
+            self.instance.driver.find_element,
+            By.XPATH,
+            '//div[@id="qc-cmp2-ui"]',
+        )
 
 
 unittest.main()
